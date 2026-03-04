ada11d4edd12a64ebae60809d85bf98090ede1b0
feng_mac <1226985060@qq.com>
2026-03-04 16:59:01 +0800

    Initial Commit

---
+++ WPJ_IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WPJ_IOS
+//
+//  Created by feng on 2026/3/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WPJ_IOS/WPJ_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WPJ_IOSApp.swift
+//  WPJ_IOS
+//
+//  Created by feng on 2026/3/4.
+//
+
+import SwiftUI
+
+@main
+struct WPJ_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
